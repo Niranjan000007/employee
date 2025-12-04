7776209d21c3cd9a6d1be14ba8e2c546619bc905
Niranjan000007 <niranjanappu192@gmail.com>
2025-12-04 20:36:00 +0530

    Sixth Commit

--- employee.h
+++ employee.h
@@ -17,8 +17,11 @@ typedef struct {
 
 int menu();
 float calculateGross(float basic, float hra, float deductions);
-int addEmployee(Employee emp[], int count);      // writes to file directly
-void displaySavedFile();                         // reads from file
+int addEmployee(Employee emp[], int count);
+void displaySavedFile();
+int deleteEmployee(Employee emp[], int count);
+int updateEmployee(Employee emp[], int count);
+void saveAllToFile(Employee emp[], int count);
 
 #endif
 
--- menu.c
+++ menu.c
@@ -9,15 +9,14 @@ int menu() {
     printf("\n====== Employee Payroll System ======\n");
     printf("1. Add Employee\n");
     printf("2. Display Employees\n");
-    printf("3. Exit\n");
+    printf("3. Delete Employee\n");
+    printf("4. Update Employee\n");
+    printf("5. Exit\n");
 
     while (1) {
         printf("Enter your choice: ");
 
-        if (!fgets(buf, sizeof(buf), stdin)) {
-            printf("Invalid input! Try again.\n");
-            continue;
-        }
+        if (!fgets(buf, sizeof(buf), stdin)) continue;
 
         int valid = 1;
         for (int i = 0; buf[i] && buf[i] != '\n'; i++)
